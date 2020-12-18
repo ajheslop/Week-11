92459af1d0135895998ca4209043ba5a3e095018
Heslop <aheslop@dxc.com>
2020-12-18 15:51:31 +0000

    initial commit

--- MainSite/MainSite/settings.py
+++ MainSite/MainSite/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for MainSite project.
 
@@ -9,18 +10,21 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jlp=im)e6w9hsws1xf3fu^hkxu9rzzyx@_53bzc3k*yi5j5zp5'
+SECRET_KEY = os.environ.get('SECRET')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
